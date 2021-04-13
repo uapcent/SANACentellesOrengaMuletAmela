44a9f0f312a90db372e891143a09f50a066a213c
JAUME <al375928@uji.es>
2021-04-13 11:25:23 +0200

    He puesto los @Repository en los dao

--- src/main/java/es/uji/ei102720coma/SANA/dao/ReservaDao.java
+++ src/main/java/es/uji/ei102720coma/SANA/dao/ReservaDao.java
@@ -4,11 +4,13 @@ import es.uji.ei102720coma.SANA.model.Reserva;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ReservaDao {
     private JdbcTemplate jdbcTemplate;
 
--- src/main/java/es/uji/ei102720coma/SANA/dao/ReservaZonaDao.java
+++ src/main/java/es/uji/ei102720coma/SANA/dao/ReservaZonaDao.java
@@ -4,11 +4,13 @@ import es.uji.ei102720coma.SANA.model.ReservaZona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ReservaZonaDao {
     private JdbcTemplate jdbcTemplate;
 
