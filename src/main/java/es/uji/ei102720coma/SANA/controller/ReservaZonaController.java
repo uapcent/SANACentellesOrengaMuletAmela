95fe491238d2bd6d06eedbe7a7116815f2007fba
uapcent <uapcent@gmail.com>
2021-05-01 15:43:12 +0200

    Primera versió del Template

--- src/main/java/es/uji/ei102720coma/SANA/controller/ReservaZonaController.java
+++ src/main/java/es/uji/ei102720coma/SANA/controller/ReservaZonaController.java
@@ -1,11 +1,17 @@
 package es.uji.ei102720coma.SANA.controller;
 
 import es.uji.ei102720coma.SANA.dao.ReservaZonaDao;
+import es.uji.ei102720coma.SANA.model.ReservaZona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/reservazona")
@@ -22,5 +28,33 @@ public class ReservaZonaController {
     public String listReservasZonas(Model model) {
         model.addAttribute("reservaszonas", reservaZonaDao.getReservasZonas());
         return "reservazona/list";
+    }
+
+    @RequestMapping(value="/add")
+    public String addReservaZona(Model model) {
+        model.addAttribute("reservazona", new ReservaZona());
+        return "reservazona/add";
+    }
+
+    @RequestMapping(value="/add", method= RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("reservazona") ReservaZona reservaZona, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            for (Object object : bindingResult.getAllErrors()) {
+                if(object instanceof FieldError) {
+                    FieldError fieldError = (FieldError) object;
+
+                    System.out.println(fieldError.getCode());
+                }
+
+                if(object instanceof ObjectError) {
+                    ObjectError objectError = (ObjectError) object;
+
+                    System.out.println(objectError.getCode());
+                }
+            }
+            return "reservazona/add";
+        }
+        reservaZonaDao.addReservaZona(reservaZona);
+        return "redirect:list";
     }
 }
